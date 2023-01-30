c07b727bedfc26c8c07a796b229481497e4e25a3
Maciej Jablonski <maciej.jablonski.1985@gmail.com>
2023-01-31 00:05:13 +0100

    Add paged and non-paged example to skel

--- .vim/skel/skel.py
+++ .vim/skel/skel.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # j4b0l's template library
 # example Python script
 
-import sys
+import boto3
 
-print "Hello from HELLO.py"
+ready = True
+# AWS regions, to avoid copy-pastes
+def get_regions():
+    client = boto3.client('ec2', region_name='us-east-1')
+    return [i['RegionName'] for i in client.describe_regions()['Regions']]
+
+regions = get_regions()
+for r in regions:
+    client = boto3.client('s3', region_name=r)
+for region in regions:
+    ec2_client = boto3.client('ec2', region_name=region)
+    ec2_paginator = ec2_client.get_paginator('describe_instances')
+
+print("Hello from HELLO.py")
 
